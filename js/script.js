d49cba4c4f70b04203ab2e23253d2672ea9ba082
Dragan Jancikin <dragan.jancikin@gmail.com>
2020-01-31 19:21:11 +0100

    Add image loader

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 const breeds = document.querySelector(".breeds");
 const dog = document.querySelector(".dog");
 const img = document.querySelector(".dog-img");
+const loader = document.querySelector(".loader");
 
 //   - with API ................................................................
 const BREEDS_URL = "https://dog.ceo/api/breeds/list/all";
@@ -31,36 +32,46 @@ function init() {
 
 // Get image from API url and add image to html ................................
 function getDog(url){
+  // show loader and hide img before image load
+  loader.classList.add("show");
+  img.classList.remove("show");
+
   fetch(url)
-  .then(function(response){
-    return response.json();
-  })
-  .then(function(data){
-    let dogImgUrl = data.message;
-    // add image to html
-    img.src = dogImgUrl;
-  });
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(data){
+      let dogImgUrl = data.message;
+      img.src = dogImgUrl; // add image to html 
+      
+      img.addEventListener('load', function (event) {
+        // hide loader and show image after image load
+        loader.classList.remove("show");
+        img.classList.add("show");
+      });
+      
+    });
 }
 
 // Get breeds from dog.ceo API and fill select tag with breeds .................
 function getBreeds(url){
   fetch(url)
-  .then(function(response){
-    return response.json();
-  })
-  .then(function(data){
-    const breedsObj = data.message;
-    const breedsArr = Object.keys(breedsObj); // Transform Object to Array
-    for (let i = 0; i < breedsArr.length; i++){
-      let option = document.createElement("option"); // Create option element
-      // Add text to option with capitalize first letter
-      option.innerText = breedsArr[i].charAt(0).toUpperCase() + breedsArr[i].slice(1);
-      // Add property value to option
-      option.value = breedsArr[i];
-      // Add option element to breeds(<select>)
-      breeds.appendChild(option);
-    }
-  });
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(data){
+      const breedsObj = data.message;
+      const breedsArr = Object.keys(breedsObj); // Transform Object to Array
+      for (let i = 0; i < breedsArr.length; i++){
+        let option = document.createElement("option"); // Create option element
+        // Add text to option with capitalize first letter
+        option.innerText = breedsArr[i].charAt(0).toUpperCase() + breedsArr[i].slice(1);
+        // Add property value to option
+        option.value = breedsArr[i];
+        // Add option element to breeds(<select>)
+        breeds.appendChild(option);
+      }
+    });
 }
 
 
