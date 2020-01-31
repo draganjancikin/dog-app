6dbb985694de269d44ce362f24ed24eb123b2a9a
Dragan Jancikin <dragan.jancikin@gmail.com>
2020-01-31 17:30:04 +0100

    Define JS variables

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Variables ===================================================================
+//   - with HTML elements ......................................................
+const breeds = document.querySelector(".breeds");
+const dog = document.querySelector(".dog");
+
+//   - with API ................................................................
+const BREEDS_URL = "https://dog.ceo/api/breeds/list/all";
+const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+
