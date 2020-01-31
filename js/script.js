683cfe32bfe2b0af6d326fa5b3bfba90a91d22b6
Dragan Jancikin <dragan.jancikin@gmail.com>
2020-01-31 17:41:33 +0100

    Fill select tag with breeds from dog.ceo API

--- js/script.js
+++ js/script.js
@@ -7,3 +7,21 @@ const dog = document.querySelector(".dog");
 const BREEDS_URL = "https://dog.ceo/api/breeds/list/all";
 const DOG_URL = "https://dog.ceo/api/breeds/image/random";
 
+// Fill select tag with breeds =================================================
+fetch(BREEDS_URL)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(data){
+    const breedsObj = data.message;
+    const breedsArr = Object.keys(breedsObj); // Transform Object to Array
+    for (let i = 0; i < breedsArr.length; i++){
+      let option = document.createElement("option"); // Create option element
+      // Add text to option with capitalize first letter
+      option.innerText = breedsArr[i].charAt(0).toUpperCase() + breedsArr[i].slice(1);
+      // Add property value to option
+      option.value = breedsArr[i];
+      // Add option element to breeds(<select>)
+      breeds.appendChild(option);
+    }
+  });
